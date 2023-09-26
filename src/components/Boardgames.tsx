2be7288acb1669c1be8a96e0bd0ae002306fdb26
Jiwon Oh <64193469+jiwon-o@users.noreply.github.com>
2023-09-26 23:07:22 +0900

    ♻️ refactor: useAsync 커스텀훅 생성하여 분리

--- src/components/Boardgames.tsx
+++ src/components/Boardgames.tsx
@@ -1,52 +1,13 @@
-import { useState, useEffect, useReducer } from "react";
 import axios from "axios";
-import { Game, State, Action } from "../types";
+import useAsync from "../hooks/useAsync";
 
-function reducer(state: State, action: Action) {
-  switch (action.type) {
-    case "LOADING":
-      return {
-        loading: true,
-        data: null,
-        error: null,
-      };
-    case "SUCCESS":
-      return {
-        loading: false,
-        data: action.data,
-        error: null,
-      };
-    case "ERROR":
-      return {
-        loading: false,
-        data: null,
-        error: action.error,
-      };
-    default:
-      return state;
-  }
+async function getGames() {
+  const response = await axios.get("http://localhost:3001/game");
+  return response.data;
 }
 
 export default function Boardgames() {
-  const [state, dispatch] = useReducer(reducer, {
-    loading: false,
-    data: null,
-    error: null,
-  });
-
-  const fetchGames = async () => {
-    dispatch({ type: "LOADING" });
-    try {
-      const response = await axios.get<Game[]>("http://localhost:3001/game");
-      dispatch({ type: "SUCCESS", data: response.data });
-    } catch (e: any) {
-      dispatch({ type: "ERROR", error: e });
-    }
-  };
-
-  useEffect(() => {
-    fetchGames();
-  }, []);
+  const state = useAsync(getGames, []);
 
   const { loading, data: games, error } = state;
 
