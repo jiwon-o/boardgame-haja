181adb4862e4e1f1e6031f0b0536447edb5ee89c
오지원 <64193469+jiwon-o@users.noreply.github.com>
2023-09-27 18:02:23 +0900

    ⚙️ config: styled-components, styled-reset 설치 후 GlobalStyle 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import "./App.css";
 import Boardgames from "./components/Boardgames";
+import GlobalStyle from "./styles/GlobalStyle";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <Boardgames />
     </div>
   );
