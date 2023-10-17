0cdef43d420597c7c70f9cd28efdb62945c618ab
오지원 <64193469+jiwon-o@users.noreply.github.com>
2023-10-17 23:55:31 +0900

    🎨 style: 보드게임 정보 카드 디자인

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -14,7 +14,9 @@ const CardWrapper = styled.div`
 `;
 
 const CardContainer = styled.div`
-  flex: 3;
+  flex: 4;
+  box-shadow: 0 0 10px red;
+
   ul {
     display: flex;
     flex-direction: column;
@@ -22,12 +24,55 @@ const CardContainer = styled.div`
 
   li {
     display: flex;
+    box-shadow: 0 0 10px green;
   }
+`;
+
+const CardThumbnail = styled.div`
+  flex: 1;
+  max-width: 250px;
+  padding: 14px;
+  box-shadow: 0 0 10px blue;
 
   img {
-    width: 200px;
-    height: 200px;
+    width: 100%;
+    height: 100%;
+    aspect-ratio: 1 / 1;
+    border-radius: 12px;
   }
+`;
+
+const CardDetail = styled.div`
+  flex: 2;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin: 14px;asdfasdfasdasdasdasdasdfasdfaasdfasdfasdfasdfasdfasdasdfasdfasdf
+`;
+
+const GameTheme = styled.strong`
+  width: fit-content;
+  padding: 10px 14px;
+  border-radius: 10px;
+  background-color: #24244a;
+  color: #ececf1;
+  font-size: 1.2rem;
+  font-weight: 700;
+`;
+
+const GameTitle = styled.h3`
+  font-size: 2rem;
+
+  span {
+    display: inline-block;
+    font-size: 1.6rem;
+    margin-left: 6px;
+  }
+`;
+
+const GameSubTitle = styled.h4`
+  font-size: 1.6rem;
+  color: #7d7b9f;
 `;
 
 interface Props {
@@ -47,27 +92,31 @@ export default function Card({ loading, error, games }: Props) {
           return (
             <ul>
               <li>
-                <img
-                  src="https://boardlife.co.kr/data/boardgame_strategy/2021/01/29/1611914795-315436.jpg"
-                  alt="게임 이미지"
-                />
-                <div>
-                  <strong>전략</strong>
-                  <h3>
-                    글룸헤이븐<span>2022</span>
-                  </h3>
+                <CardThumbnail>
+                  <img src={game.image} alt="보드게임 이미지" />
+                </CardThumbnail>
+                <CardDetail>
+                  <GameTheme>{game.theme}</GameTheme>
+                  <GameTitle>
+                    {game.name}
+                    <span>({game.releaseYear})</span>
+                  </GameTitle>
+                  <GameSubTitle>{game.subTitle}</GameSubTitle>
                   <div>
                     <span>
-                      인원: <strong>4 ~ 8 명</strong>
+                      인원:{" "}
+                      <strong>
+                        {game.min_player} ~ {game.max_player}명
+                      </strong>
                     </span>
                     <span>
-                      평점: <strong>9.05</strong>
+                      평점: <strong>{game.rate}점</strong>
                     </span>
                     <span>
-                      플레이 타임: <strong>15분</strong>
+                      플레이 타임: <strong>{game.play_time}</strong>
                     </span>
                   </div>
-                </div>
+                </CardDetail>
               </li>
             </ul>
           );
