45787f85fe6ec4ce283bb5b255c6909868625e7f
오지원 <64193469+jiwon-o@users.noreply.github.com>
2023-10-30 19:08:46 +0900

    ✨ feat: react-js-pagination 구현

--- src/components/commons/Card.tsx
+++ src/components/commons/Card.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Game } from "../../types";
 import styled from "styled-components";
 import { IoPeopleSharp } from "react-icons/io5";
@@ -7,6 +7,8 @@ import { BiSolidTimeFive } from "react-icons/bi";
 import { TbRating12Plus } from "react-icons/tb";
 import { useNavigate } from "react-router-dom";
 import AsideNav from "./Aside/AsideNav";
+import Pagination from "react-js-pagination";
+import "../../styles/pagination.css";
 
 const CardWrapper = styled.div`
   display: flex;
@@ -94,11 +96,11 @@ const GamePlay = styled.ul`
   display: flex;
   flex-wrap: wrap;
   margin-top: 10px;
-  gap: 12px;
+  gap: 8px;
 `;
 
 const GamePlayItem = styled.li`
-  padding: 10px 12px;
+  padding: 8px 12px;
   border-radius: 10px;
   background-color: #382f84;
   color: #ececf1;
@@ -125,7 +127,7 @@ const GameRank = styled.span`
   border-radius: 50%;
   font-size: 1.4rem;
   color: white;
-  background-color: #382f84;
+  background-color: #606efc;
   opacity: 0;
 
   ${CardItem}:hover & {
@@ -143,14 +145,28 @@ export default function Card({ loading, error, games }: Props) {
   const navigate = useNavigate();
   const [selectedTheme, setSelectedTheme] = useState("all");
   const themes = [...new Set(games?.map((game) => game.theme))];
+  const [page, setPage] = useState<number>(1);
 
-  const filteredGames = games?.filter((game) => {
+  const CardPerPage: number = 6;
+  const indexOfLastCard: number = page * CardPerPage;
+  const indexOfFirstCard: number = indexOfLastCard - CardPerPage;
+  const [currentGames, setCurrentGames] = useState<Game[] | null>(null);
+
+  const handlePageChange = (page: number) => {
+    setPage(page);
+  };
+
+  const filteredGames = games!.filter((game) => {
     if (selectedTheme === "all") {
       return true; // 모든 게임 표시
     } else {
       return game.theme === selectedTheme; // 선택한 카테고리와 일치하는 게임만 표시
     }
   });
+
+  useEffect(() => {
+    setCurrentGames(filteredGames.slice(indexOfFirstCard, indexOfLastCard));
+  }, [filteredGames, page]);
 
   const handleCardItemClick = (game: Game) => {
     navigate(`/game/${game.id}`, { state: { game } });
@@ -166,7 +182,7 @@ export default function Card({ loading, error, games }: Props) {
   return (
     <CardWrapper>
       <CardContainer>
-        {filteredGames?.map((game) => {
+        {currentGames?.map((game) => {
           return (
             <CardItems>
               <CardItem onClick={() => handleCardItemClick(game)}>
@@ -202,6 +218,16 @@ export default function Card({ loading, error, games }: Props) {
             </CardItems>
           );
         })}
+        <Pagination
+          hideDisabled
+          activePage={page}
+          itemsCountPerPage={CardPerPage}
+          totalItemsCount={filteredGames.length}
+          pageRangeDisplayed={5}
+          prevPageText={"<"}
+          nextPageText={">"}
+          onChange={handlePageChange}
+        />
       </CardContainer>
       <AsideNav>
         <h2>카테고리 별 버튼</h2>
