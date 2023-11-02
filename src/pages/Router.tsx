5ef8de33931a9373f54b8d45cc4cebb82a7d5333
오지원 <64193469+jiwon-o@users.noreply.github.com>
2023-11-02 16:52:17 +0900

    🚚 rename: 페이지 파일 이름 변경

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Home";
-import Detail from "./Detail";
+import Home from "./HomePage";
+import Detail from "./DetailPage";
 import AllGames from "./AllGames";
 
 export default function AppRouter() {
