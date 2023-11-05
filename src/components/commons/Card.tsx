356a70a6ddde06bed2fe6907a9a15b91f2ace1d3
Jiwon Oh <64193469+jiwon-o@users.noreply.github.com>
2023-11-05 17:15:03 +0900

    ✨ feat: 평점에 따른 게임 필터 구현(Checkbox)

--- src/components/commons/Card.tsx
+++ src/components/commons/Card.tsx
@@ -13,7 +13,7 @@ import Checkbox from "./Checkbox/Checkbox";
 
 const CardWrapper = styled.div`
   display: flex;
-  justify-content: center;
+  align-items: flex-start;
 `;
 
 const CardContainer = styled.div`
@@ -46,13 +46,14 @@ const CardItem = styled.li`
 const CardThumbnail = styled.div`
   flex: 1;
   max-width: 240px;
+  min-height: 240px;
   padding: 14px;
   border-right: 1px solid #14112e;
 
   img {
     width: 100%;
     height: 100%;
-    aspect-ratio: 1 / 1;
+    object-fit: cover;
     border-radius: 12px;
   }
 `;
@@ -141,12 +142,10 @@ const GameRank = styled.span`
 
 const AsideContainer = styled.aside`
   position: sticky;
-  overflow-y: auto;
   top: 20px;
   z-index: 2;
   flex: 1;
   max-width: 300px;
-  max-height: 300px;
   min-width: 200px;
   border-radius: 6px;
   padding: 12px;
@@ -154,14 +153,45 @@ const AsideContainer = styled.aside`
   box-shadow: 3px 3px 5px #14112e;
 `;
 
-const AsideLists = styled.div``;
+const AsideLists = styled.div`
+  strong {
+    display: inline-block;
+    margin-bottom: 12px;
+  }
+`;
 
 const AsideItem = styled.div`
-  display: flex;
+  /* display: flex;
+  justify-content: center;
   align-items: center;
-
   flex-wrap: wrap;
+  gap: 4px;
+
+  input {
+    display: none;
+  }
+
+  label {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-width: 40px;
+    min-height: 24px;
+    padding: 5px 10px;
+    border: 1px solid black;
+    border-radius: 3px;
+  }
+
+  label:hover {
+    cursor: pointer;
+  }
+
+  input[type="radio"]:checked + label {
+    background: #ccc;
+    color: white;
+  } */
 `;
+
 const AsideRadio = styled.div``;
 
 interface Props {
@@ -176,6 +206,7 @@ export default function Card({ loading, error, games }: Props) {
   const [selectedPlayerCounts, setSelectedPlayerCounts] = useState<string[]>(
     []
   );
+  const [selectedRating, setSelectedRating] = useState<string[]>([]);
 
   const [filteredGames, setFilteredGames] = useState(games!);
 
@@ -192,25 +223,45 @@ export default function Card({ loading, error, games }: Props) {
   useEffect(() => {
     setFilteredGames(
       games!.filter((game) => {
+        // 게임 인원 필터
         const minPlayer = parseInt(game.min_player, 10);
         const maxPlayer = parseInt(game.max_player, 10);
 
-        if (selectedPlayerCounts.length === 0) {
-          return true;
-        } else {
-          return selectedPlayerCounts.some((value) => {
+        const playerCountMatch =
+          selectedPlayerCounts.length === 0 ||
+          selectedPlayerCounts.some((value) => {
             if (value === "over10") {
               return minPlayer >= 10 || maxPlayer >= 10;
             } else {
-              const intValue = parseInt(value, 10);
-              return intValue >= minPlayer && intValue <= maxPlayer;
+              const selectedCount = parseInt(value, 10);
+              return selectedCount >= minPlayer && selectedCount <= maxPlayer;
             }
           });
-        }
+
+        // 게임 평점 필터
+        const gameRating = parseFloat(game.rate);
+
+        const ratingMatch =
+          selectedRating.length === 0 ||
+          selectedRating.some((value) => {
+            if (value === "under5") {
+              return gameRating < 5;
+            } else if (value === "10") {
+              return gameRating === 10;
+            } else {
+              const selectedRating = parseInt(value, 10);
+              return (
+                selectedRating <= gameRating && gameRating < selectedRating + 1
+              );
+            }
+          });
+
+        // 두 필터 모두 만족하는 게임만 반환
+        return playerCountMatch && ratingMatch;
       })
     );
     setPage(1);
-  }, [selectedPlayerCounts]);
+  }, [selectedPlayerCounts, selectedRating]);
 
   useEffect(() => {
     setCurrentGames(filteredGames!.slice(indexOfFirstCard, indexOfLastCard));
@@ -224,50 +275,59 @@ export default function Card({ loading, error, games }: Props) {
     setSelectedPlayerCounts(selectedCounts);
   };
 
+  const handleRatingChange = (selectedRating: string[]) => {
+    setSelectedRating(selectedRating);
+  };
+
   if (loading) return <div>로딩중..</div>;
   if (error) return <div>에러가 발생했습니다</div>;
   if (!games) return null;
   return (
     <CardWrapper>
       <CardContainer>
-        {currentGames?.map((game) => {
-          return (
-            <CardItems>
-              <CardItem onClick={() => handleCardItemClick(game)}>
-                <CardThumbnail>
-                  <img src={game.image} alt="보드게임 이미지" />
-                </CardThumbnail>
-                <CardDetail>
-                  <GameTitleAndTheme>
-                    <GameTheme>{game.theme}</GameTheme>
-                    <GameTitle>
-                      {game.name}
-                      <span>({game.releaseYear})</span>
-                      <GameSubTitle>{game.subTitle}</GameSubTitle>
-                    </GameTitle>
-                  </GameTitleAndTheme>
-                  <GamePlay>
-                    <GamePlayItem>
-                      <IoPeopleSharp /> {game.min_player} ~ {game.max_player}명
-                    </GamePlayItem>
-                    <GamePlayItem>
-                      <AiFillStar /> {game.rate}점
-                    </GamePlayItem>
-                    <GamePlayItem>
-                      <BiSolidTimeFive /> {game.play_time}
-                    </GamePlayItem>
-                    <GamePlayItem>
-                      <TbRating12Plus /> {game.play_age}
-                    </GamePlayItem>
-                  </GamePlay>
-                  <GameRank>{game.ranking}</GameRank>
-                </CardDetail>
-              </CardItem>
-            </CardItems>
-          );
-        })}
+        {currentGames?.length === 0 ? (
+          <div>해당 분류와 일치하는 보드게임이 없습니다.</div>
+        ) : (
+          currentGames?.map((game) => {
+            return (
+              <CardItems>
+                <CardItem onClick={() => handleCardItemClick(game)}>
+                  <CardThumbnail>
+                    <img src={game.image} alt="보드게임 이미지" />
+                  </CardThumbnail>
+                  <CardDetail>
+                    <GameTitleAndTheme>
+                      <GameTheme>{game.theme}</GameTheme>
+                      <GameTitle>
+                        {game.name}
+                        <span>({game.releaseYear})</span>
+                        <GameSubTitle>{game.subTitle}</GameSubTitle>
+                      </GameTitle>
+                    </GameTitleAndTheme>
+                    <GamePlay>
+                      <GamePlayItem>
+                        <IoPeopleSharp /> {game.min_player} ~ {game.max_player}
+                        명
+                      </GamePlayItem>
+                      <GamePlayItem>
+                        <AiFillStar /> {game.rate}점
+                      </GamePlayItem>
+                      <GamePlayItem>
+                        <BiSolidTimeFive /> {game.play_time}
+                      </GamePlayItem>
+                      <GamePlayItem>
+                        <TbRating12Plus /> {game.play_age}
+                      </GamePlayItem>
+                    </GamePlay>
+                    <GameRank>{game.ranking}</GameRank>
+                  </CardDetail>
+                </CardItem>
+              </CardItems>
+            );
+          })
+        )}
+
         <Pagination
-          hideDisabled
           activePage={page}
           itemsCountPerPage={CardPerPage}
           totalItemsCount={filteredGames.length}
@@ -280,7 +340,7 @@ export default function Card({ loading, error, games }: Props) {
       <AsideContainer>
         <h2 className="a11y">인원수, 평점, 플레이 시간에 따른 filter table</h2>
         <CheckboxGroup
-          label="인원 수"
+          label="게임 인원"
           values={selectedPlayerCounts}
           onChange={handlePlayerCountsChange}>
           {["1", "2", "3", "4", "5", "6", "7", "8", "9"].map((playerCount) => (
@@ -292,7 +352,21 @@ export default function Card({ loading, error, games }: Props) {
             10명 이상
           </Checkbox>
         </CheckboxGroup>
-        <footer>[{selectedPlayerCounts.join(",")}]을 좋아하시군요!</footer>
+        <CheckboxGroup
+          label="게임 평점"
+          values={selectedRating}
+          onChange={handleRatingChange}>
+          {["10", "9", "8", "7", "6", "5"].map((rating) => (
+            <Checkbox id={`rating-${rating}`} value={rating}>
+              {parseInt(rating) === 10
+                ? `${rating}점`
+                : `${rating}-${parseInt(rating) + 1}점`}
+            </Checkbox>
+          ))}
+          <Checkbox id={`player-under5`} value="under5">
+            5점 미만
+          </Checkbox>
+        </CheckboxGroup>
       </AsideContainer>
     </CardWrapper>
   );
