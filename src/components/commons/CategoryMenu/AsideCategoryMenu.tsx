fce0563d74516dbf4b58f8c67f6cd084b3ad9699
Jiwon Oh <64193469+jiwon-o@users.noreply.github.com>
2023-11-18 23:59:10 +0900

    ✨ feat: AsideCategoryNav 컴포넌트로 분리

---
+++ src/components/commons/CategoryMenu/AsideCategoryMenu.tsx
@@ -0,0 +1,25 @@
+import React, { ReactNode } from "react";
+import styled from "styled-components";
+
+const AsideMenuBox = styled.aside`
+  position: sticky;
+  overflow-y: auto;
+  top: 20px;
+  z-index: 2;
+  flex: 1;
+  max-width: 300px;
+  max-height: 300px;
+  min-width: 200px;
+  border-radius: 6px;
+  padding: 12px;
+  background-color: #1f1a4cd7;
+  box-shadow: 3px 3px 5px #14112e;
+`;
+
+interface Props {
+  children: ReactNode;
+}
+
+export default function AsideCategoryMenu({ children }: Props) {
+  return <AsideMenuBox>{children}</AsideMenuBox>;
+}
