a583c4d250f6166701863c197f61ba2e7396f9ec
Jiwon Oh <64193469+jiwon-o@users.noreply.github.com>
2023-12-02 23:29:04 +0900

    ♻️ refactor: 불필요한 코드 삭제

--- src/components/commons/Gallery.tsx
+++ src/components/commons/Gallery.tsx
@@ -6,8 +6,6 @@ import { Game } from "../../types";
 import useSearch from "../../hooks/useSearch";
 import useScroll from "../../hooks/useScroll";
 import { useState } from "react";
-import Modal from "./Modal/Modal";
-import Detail from "../../pages/DetailPage";
 import { useNavigate } from "react-router-dom";
 
 const rankColors: { [key: number]: string } = {
@@ -34,7 +32,7 @@ const MasonryContainer = styled(Masonry)`
   }
 `;
 
-const CardContainer = styled.div`
+const GameCardContainer = styled.div`
   max-width: 224px;
   position: relative;
   background-color: transparent;
@@ -49,7 +47,7 @@ const CardContainer = styled.div`
   }
 `;
 
-const Card = styled.div`
+const GameCard = styled.div`
   position: relative;
   border-radius: 6px;
   box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.5);
@@ -80,16 +78,16 @@ const GameRank = styled.span<GameRankProps>`
   display: flex;
   justify-content: center;
   align-items: center;
-  min-width: 34px;
-  min-height: 34px;
-  padding: 0 6px 0 5px;
+  min-width: 28px;
+  min-height: 28px;
   border-radius: 50%;
-  font-size: 1.8rem;
+  padding: 2px;
+  font-size: 1.4rem;
   color: white;
   opacity: 0;
   background-color: ${(props) => rankColors[props.ranking] || "#e9b824"};
 
-  ${CardContainer}:hover & {
+  ${GameCardContainer}:hover & {
     opacity: 1;
   }
 `;
@@ -102,7 +100,7 @@ const GameImg = styled.img`
   object-fit: cover;
 `;
 
-const GameTitle = styled.h4`
+const GameTitle = styled.h3`
   display: flex;
   justify-content: space-between;
   gap: 6px;
@@ -125,6 +123,12 @@ export default function Gallery({ loading, error, games, searchGame }: Props) {
   const dataCount = useScroll(50);
   const navigate = useNavigate();
 
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
+
   const filteredGames = games?.filter((game) => {
     return (
       searchGames(searchGame, game.name) ||
@@ -138,15 +142,6 @@ export default function Gallery({ loading, error, games, searchGame }: Props) {
 
   const pagedGames = filteredGames?.slice(0, dataCount);
 
-  const generateImageUrl = (imageUrl: string, size: number) => {
-    if (imageUrl.endsWith(".jpg")) {
-      return imageUrl.replace(".jpg", `_N_${size}x${size}_100_5_.jpg`);
-    } else if (imageUrl.endsWith(".png")) {
-      return imageUrl.replace(".png", `_N_${size}x${size}_100_5_.png`);
-    }
-    return imageUrl;
-  };
-
   if (loading) return <div>로딩중..</div>;
   if (error) return <div>에러가 발생했습니다</div>;
   if (!games) return null;
@@ -158,36 +153,26 @@ export default function Gallery({ loading, error, games, searchGame }: Props) {
           className="list"
           columnClassName="column">
           {pagedGames.map((game: Game) => {
-            // const imageUrl = generateImageUrl(game.image, 200);
-
             return (
-              <CardContainer key={game.id}>
-                <Card onClick={() => handleCardClick(game)}>
+              <GameCardContainer key={game.id}>
+                <GameCard onClick={() => handleCardClick(game)}>
                   <GameRank ranking={game.ranking}>{game.ranking}</GameRank>
                   <GameImg
-                    src={
-                      // game.image.includes("boardlife.co.kr")
-                      //   ? imageUrl
-                      //   : game.image
-                      game.image
-                    }
+                    src={game.image}
                     alt={game.name}
+                    onLoad={handleImageLoad}
+                    style={{ display: imageLoaded ? "block" : "none" }}
                   />
-                </Card>
+                </GameCard>
                 <GameTitle>{game.name}</GameTitle>
-                <h5 className="a11y">{game.subTitle}</h5>
-              </CardContainer>
+                <h4 className="a11y">{game.subTitle}</h4>
+              </GameCardContainer>
             );
           })}
         </MasonryContainer>
       ) : (
         <p>보드게임을 찾지 못했습니다.</p>
       )}
-      {/* {isModalOpen && (
-        <Modal isOpen={isModalOpen} onClose={closeModal}>
-          {selectedGame && <Detail />}
-        </Modal>
-      )} */}
     </GamesLayout>
   );
 }
