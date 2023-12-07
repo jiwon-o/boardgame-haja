ee345ef60cf4f6abe0ed3e64710fe3d9ad7659a3
Jiwon Oh <64193469+jiwon-o@users.noreply.github.com>
2023-12-07 23:52:40 +0900

    ♻️ refactor: 라우터 url 수정 및 로그인 페이지 라우터 추가

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -2,17 +2,17 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./HomePage";
 import Detail from "./DetailPage";
-import AllGames from "./AllGames";
-import CategoriesPage from "./CategoriesPage";
+import CategoryPage from "./CategoryPage";
+import LoginPage from "./LoginPage";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/boardgame" element={<AllGames />} />
         <Route path="/boardgame/:gameId" element={<Detail />} />
-        <Route path="/boardgame/categories" element={<CategoriesPage />} />
+        <Route path="/categories/:theme" element={<CategoryPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
