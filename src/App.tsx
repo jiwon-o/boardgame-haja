d536dd80c80c34448febf19f02a21fae7ead6f1c
Jiwon Oh <64193469+jiwon-o@users.noreply.github.com>
2024-01-06 22:41:22 +0900

    🎨 style: 공통 css를 위해 ThemeProvider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import GlobalStyle from "./styles/GlobalStyle";
-import AppRouter from "./pages/Router";
-import styled from "styled-components";
+import GlobalStyle from './styles/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
+import AppRouter from './pages/Router';
+import styled from 'styled-components';
 
 const AppWrapper = styled.div`
   padding: 0 40px;
@@ -10,7 +12,9 @@ function App() {
   return (
     <AppWrapper>
       <GlobalStyle />
-      <AppRouter />
+      <ThemeProvider theme={theme}>
+        <AppRouter />
+      </ThemeProvider>
     </AppWrapper>
   );
 }
---
+++ src/styles/theme.ts
@@ -0,0 +1,40 @@
+const colors = {
+  mainColor: '#606efc',
+  navyColor: '#382f84',
+  whiteColor: '#ececf1',
+  blackColor: '#1b1d23',
+  darkGrayColor: '#24244a',
+  grayColor: '#383d61',
+  lightGrayColor: '#666666',
+  blueColor: '#1d40da',
+  skyblueColor: '#3d80bb',
+  txtSubColor: '#7d7b9f',
+  txtParaColor: '#c5c3ec',
+  bgGradientColor1: '#161338',
+  bgGradientColor2: '#1c174b',
+  bgHoverColor: '#1f1a4c',
+  bdBlueColor: '#3e60f5',
+  bdNavyColor: '#42389e',
+  lightBlackColor: '#14112e',
+  redColor: '#ff4545',
+  greenColor: '#008000',
+  yellowColor: '#ffff00',
+};
+
+const fontSize = {
+  base: '1rem',
+  xs: '1.2rem',
+  sm: '1.4rem',
+  md: '1.6rem',
+  lg: '1.8rem',
+  xl: '2rem',
+  sub: '2.4rem',
+  ttl: '3rem',
+};
+
+const theme = {
+  colors,
+  fontSize,
+};
+
+export default theme;
